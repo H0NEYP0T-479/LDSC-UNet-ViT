6b77e5250e9beb69e751ef889502c22501c7ccdc
H0NEYP0T-479 <Fa-23-bscs-481@lgu.edu.pk>
2026-06-03 09:57:02 +0500

    fix: autocast updated, dataset.zip ignored

--- backend/app/models/unet/model.py
+++ backend/app/models/unet/model.py
@@ -109,11 +109,19 @@ class UNetLung(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x1 = self.inc(x)
         
+
+
+
+        
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         
+
+
+
+
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
